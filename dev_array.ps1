4a4c1c80a5bdab99d7ca8fa6f1e165a85b7344d0
Rob Hux <rhux@array-advisors.com>
2018-06-09 14:49:29 -0400

    creating an array script

---
+++ dev_array.ps1
@@ -0,0 +1,163 @@
+# Description: Boxstarter Script  
+# Author: Microsoft
+# Common dev settings for desktop app development
+
+Disable-UAC
+
+#--- Windows Subsystems/Features ---
+choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+choco install -y Microsoft-Hyper-V-All -source windowsFeatures
+#choco install -y docker-for-windows
+
+#--- Configuring Windows properties ---
+#--- Windows Features ---
+Set-TaskbarSmall
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+#--- File Explorer Settings ---
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+# open file explorer to "this pc"
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+# Show taskbar on all monitors
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarEnabled  -Value 1
+# Show taskbar buttons on taskbar where window is open
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+#--- Enabling developer mode on the system ---
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+#--- Ubuntu ---
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.appx -UseBasicParsing
+
+
+#--- VS 2017 uwp and azure workloads + git tools ---
+# See this for install args: https://chocolatey.org/packages/VisualStudio2017Professional
+# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community 
+# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
+choco install -y visualstudio2017professional --package-parameters "--add Microsoft.VisualStudio.Workload.CoreEditor --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.Net.ComponentGroup.TargetingPacks.Common --add Microsoft.Component.Azure.DataLake.Tools --add Microsoft.VisualStudio.Component.Azure.ResourceManager.Tools --add Microsoft.VisualStudio.Component.DiagnosticTools --add Microsoft.VisualStudio.ComponentGroup.Azure.CloudServices --add Microsoft.VisualStudio.ComponentGroup.Azure.ResourceManager.Tools --add Microsoft.VisualStudio.Component.DiagnosticTools --add Microsoft.VisualStudio.ComponentGroup.Web.CloudTools --add Microsoft.VisualStudio.Component.AppInsights.Tools --add Microsoft.VisualStudio.Component.Wcf.Tooling --add Microsoft.Net.Component.4.6.2.SDK --add Microsoft.Net.Component.4.7.SDK --add Microsoft.Net.Component.4.7.1.SDK --add Microsoft.Net.Component.4.6.2.TargetingPack --add Microsoft.Net.Component.4.7.TargetingPack --add Microsoft.Net.Component.4.7.1.TargetingPack --add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools --add Microsoft.Net.ComponentGroup.4.7.DeveloperTools --add Microsoft.Net.ComponentGroup.4.7.1.DeveloperTools --add Microsoft.VisualStudio.Component.Azure.Storage.AzCopy --add Microsoft.VisualStudio.Component.AspNet45 --add Microsoft.VisualStudio.Component.DiagnosticTools --add Microsoft.VisualStudio.Component.Azure.Waverton --add Microsoft.VisualStudio.Component.Azure.Waverton.BuildTools --add Microsoft.VisualStudio.Component.TypeScript.2.1 --add Microsoft.VisualStudio.Component.TypeScript.2.2 --add Microsoft.VisualStudio.Component.TypeScript.2.5 --add Microsoft.VisualStudio.Component.TypeScript.2.6 --add Microsoft.VisualStudio.Component.TestTools.Core --add Microsoft.VisualStudio.Component.Git --remove Component.Redgate.SQLSearch.VSExtension --remove Component.Dotfuscator --remove Microsoft.ComponentGroup.Blend --remove Microsoft.Component.HelpViewer"
+RefreshEnv
+
+choco install chocolatey-visualstudio.extension
+RefreshEnv
+
+choco install -y KB2919355 KB2919442 KB2999226 KB3033929 KB3035131 chocolatey-windowsupdate.extension chocolatey-core.extension vcredist140 vcredist2010 vcredist2015
+RefreshEnv
+
+choco install -y git -params '"/GitOnlyOnPath /WindowsTerminal"'
+RefreshEnv
+choco install -y googlechrome Firefox
+RefreshEnv
+
+choco install -y 7zip 7zip.install curl cmder FiraCode kdiff3 keepass keepass.instal nodejs nodejs.install paint.net notepadplusplus notepadplusplus.install poshgit 
+choco install -y SourceTree resharper sysinternals yarn
+RefreshEnv
+
+choco install -y visualstudiocode
+RefreshEnv
+
+code --install-extension aureliaeffect.aurelia
+code --install-extension behzad88.aurelia
+code --install-extension steoates.autoimport
+code --install-extension jerryhong.autofilename
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-azuretools.vscode-azureappservice
+code --install-extension ms-vscode.azurecli
+code --install-extension ms-vscode.csharp
+code --install-extension fknop.vscode-npm
+code --install-extension alefragnani.project-manager
+code --install-extension xabikos.javascriptsnippets
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension eg2.tslint
+code --install-extension ms-vsliveshare.vsliveshare
+
+
+# Disable Xbox Gamebar
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWord -Value 0
+
+# Turn off People in Taskbar
+If (-Not (Test-Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People")) {
+    New-Item -Path HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People | Out-Null
+}
+Set-ItemProperty -Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name PeopleBand -Type DWord -Value 0
+
+
+
+#--- Remove windows "features"
+# 3D Builder
+Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage
+
+# Bing Weather, News, Sports, and Finance (Money):
+Get-AppxPackage Microsoft.BingFinance | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingSports | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
+
+# BubbleWitch
+Get-AppxPackage *BubbleWitch* | Remove-AppxPackage
+
+# Candy Crush
+Get-AppxPackage king.com.CandyCrush* | Remove-AppxPackage
+Get-AppxPackage flaregamesGmbH.RoyalRevolt2 | Remove-AppxPackage
+
+Get-AppxPackage *FarmVille2CountryEscape* | Remove-AppxPackage
+Get-AppxPackage *Facebook* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage *MSPaint* | Remove-AppxPackage
+Get-AppxPackage *Netflix* | Remove-AppxPackage
+Get-AppxPackage *photos* | Remove-AppxPackage
+Get-AppxPackage *PandoraMediaInc* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *Twitter* | Remove-AppxPackage
+Get-AppxPackage *XboxSpeechToTextOverlay* | Remove-AppxPackage
+Get-AppxPackage *XboxOneSmartGlass* | Remove-AppxPackage
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
+# Zune Music, Movies & TV
+Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
+# March of Empires
+Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage
+# Skype (Metro version)
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+# Disney Magic Kingdom
+Get-AppxPackage *DisneyMagicKingdom* | Remove-AppxPackage
+
+# Hidden City: Hidden Object Adventure
+Get-AppxPackage *HiddenCityMysteryofShadows* | Remove-AppxPackage
+
+
+#--- grabbing latest UWP Samples off Github ---
+RefreshEnv
+cd $env:USERPROFILE\desktop
+mkdir UwpSamples
+
+# installing Windows Template Studio VSIX
+
+
+
+# Privacy: Let apps use my advertising ID: Disable
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+  New-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo | Out-Null
+}
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo -Name Enabled -Type DWord -Value 0
+
+# WiFi Sense: HotSpot Sharing: Disable
+If (-Not (Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+  New-Item -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting | Out-Null
+}
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting -Name value -Type DWord -Value 0
+
+# WiFi Sense: Shared HotSpot Auto-Connect: Disable
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots -Name value -Type DWord -Value 0
+
+# Start Menu: Disable Bing Search Results
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+
+
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
